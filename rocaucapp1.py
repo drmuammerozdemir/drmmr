1074d7ca5a50c2a398ac27381edc92e367fa10ca
drmuammerozdemir <muammer144@hotmail.com>
2026-05-04 00:51:02 +0300

    Update rocaucapp1.py

--- rocaucapp1.py
+++ rocaucapp1.py
@@ -317,9 +317,9 @@ if df is not None and analysis_type == "Single ROC Curve":
         fpr_smooth = np.linspace(0, 1, 200)
         interp_func = interp1d(fpr, tpr, kind='linear', bounds_error=False, fill_value=(0, 1))
         tpr_smooth = np.maximum.accumulate(interp_func(fpr_smooth))
-        ax.plot(fpr_smooth, tpr_smooth, lw=2, label=...)
+        ax.plot(fpr_smooth, tpr_smooth, lw=2, label=f'{custom_name} (AUC = {roc_auc:.3f})')
     else:
-        ax.plot(fpr, tpr, lw=2, label=...)
+        ax.plot(fpr, tpr, lw=2, label=f'{custom_name} (AUC = {roc_auc:.3f})')
     ax.plot([0, 1], [0, 1], linestyle='--', color='gray')
     ax.set_xlim([0.0, 1.0])
     ax.set_ylim([0.0, 1.05])
@@ -455,9 +455,9 @@ if df is not None and analysis_type == "Multiple ROC Curves":
             fpr_smooth = np.linspace(0, 1, 200)
             interp_func = interp1d(fpr, tpr, kind='linear', bounds_error=False, fill_value=(0, 1))
             tpr_smooth = np.maximum.accumulate(interp_func(fpr_smooth))
-            ax.plot(fpr_smooth, tpr_smooth, lw=2, label=...)
+            ax.plot(fpr_smooth, tpr_smooth, lw=2, label=f"{custom_names.get(var,var)} (AUC = {my_auc:.3f})")
         else:
-            ax.plot(fpr, tpr, lw=2, label=...)
+            ax.plot(fpr, tpr, lw=2, label=f"{custom_names.get(var,var)} (AUC = {my_auc:.3f})")
 
         # METRİKLER
         best_thr_internal, _, _ = youden_best_threshold(fpr, tpr, thr_tmp)
